2a84ec78afe326af36a86e34184b29b02219f14b
edwin harley martinez gomez <alpha0202@gmail.com>
2023-10-01 19:10:52 -0500

    usuando select para proyecciones de datos, uso de clase DTO.

---
+++ EFCorePeliculas/Controllers/AutoresController.cs
@@ -0,0 +1,31 @@
+﻿using EFCorePeliculas.DTOs;
+using EFCorePeliculas.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public AutoresController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ActorDTO>> Get()
+        {
+            var actors = await _context.Actores
+                                .Select(a => new ActorDTO {Id = a.Id, Nombre = a.Nombre}).ToListAsync();   //se utiliza la clase de transporte (DTO)
+
+            return actors;
+        }
+
+
+    }
+}
--- EFCorePeliculas/Controllers/GenerosController.cs
+++ EFCorePeliculas/Controllers/GenerosController.cs
@@ -1,5 +1,6 @@
 ﻿using EFCorePeliculas.Entidades;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,10 +20,56 @@ namespace EFCorePeliculas.Controllers
         [HttpGet]
         public async Task<IEnumerable<Genero>> Get()
         {
-            return await _dbContext.Generos.ToListAsync();
+            return await _dbContext.Generos.OrderBy(g => g.Nombre).ToListAsync();
         }
 
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var genero = _dbContext.Generos.FirstOrDefaultAsync(g => g.Identificador == id);
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+            return await genero;
+        }
+
+
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Genero>> Primer()
+        {
+           var genero = await _dbContext.Generos.FirstOrDefaultAsync(g => g.Nombre.StartsWith("C"));
+
+            if (genero is null)
+            {
+                return NotFound();
+            }
+            return genero;
+        }
+
+        [HttpGet("filtrar")]
+        public async Task<IEnumerable<Genero>> Filtrar(string nombre)
+        {
+            return await _dbContext.Generos
+                            .Where(g => g.Nombre.Contains(nombre))
+                            .ToListAsync();                        //g.Nombre.StartsWith("C")|| g.Nombre.StartsWith("A")).ToListAsync();
+                                                                    //.OrderBy(g=>g.Nombre)
+        }
+
+        [HttpGet("paginacion")]
+        public async Task<ActionResult<IEnumerable<Genero>>> GetPaginacion(int pagina=1)
+        {
+            var cantidadRegistrosPorPagina = 2;
+            var generos = await _dbContext.Generos
+                                .Skip((pagina-1)*cantidadRegistrosPorPagina)
+                                .Take(cantidadRegistrosPorPagina)
+                                .ToListAsync();
+
+            return generos;
+        }
 
     }
 }
