7249eae1d77d6bb3c45a936c4634b4fe6113078a
edwin harley martinez gomez <alpha0202@gmail.com>
2024-03-16 19:05:32 -0500

    generando persistencia de datos- post generos

--- EFCorePeliculas/Controllers/GenerosController.cs
+++ EFCorePeliculas/Controllers/GenerosController.cs
@@ -17,6 +17,7 @@ namespace EFCorePeliculas.Controllers
             _dbContext = dBContext;
         }
 
+
         [HttpGet]
         public async Task<IEnumerable<Genero>> Get()
         {
@@ -38,37 +39,49 @@ namespace EFCorePeliculas.Controllers
 
 
 
-        [HttpGet("primero")]
-        public async Task<ActionResult<Genero>> Primer()
+        //[HttpGet("primero")]
+        //public async Task<ActionResult<Genero>> Primer()
+        //{
+        //   var genero = await _dbContext.Generos.FirstOrDefaultAsync(g => g.Nombre.StartsWith("C"));
+
+        //    if (genero is null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return genero;
+        //}
+
+        //[HttpGet("filtrar")]
+        //public async Task<IEnumerable<Genero>> Filtrar(string nombre)
+        //{
+        //    return await _dbContext.Generos
+        //                    .Where(g => g.Nombre.Contains(nombre))
+        //                    .ToListAsync();                        //g.Nombre.StartsWith("C")|| g.Nombre.StartsWith("A")).ToListAsync();
+        //                                                            //.OrderBy(g=>g.Nombre)
+        //}
+
+        //[HttpGet("paginacion")]
+        //public async Task<ActionResult<IEnumerable<Genero>>> GetPaginacion(int pagina=1)
+        //{
+        //    var cantidadRegistrosPorPagina = 2;
+        //    var generos = await _dbContext.Generos
+        //                        .Skip((pagina-1)*cantidadRegistrosPorPagina)
+        //                        .Take(cantidadRegistrosPorPagina)
+        //                        .ToListAsync();
+
+        //    return generos;
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Genero genero)
         {
-           var genero = await _dbContext.Generos.FirstOrDefaultAsync(g => g.Nombre.StartsWith("C"));
+            var status1 = _dbContext.Entry(genero).State;
+            _dbContext.Add(genero);
+            var status2 = _dbContext.Entry(genero).State;
+            await _dbContext.SaveChangesAsync();
+            var status3 = _dbContext.Entry(genero).State;
 
-            if (genero is null)
-            {
-                return NotFound();
-            }
-            return genero;
-        }
-
-        [HttpGet("filtrar")]
-        public async Task<IEnumerable<Genero>> Filtrar(string nombre)
-        {
-            return await _dbContext.Generos
-                            .Where(g => g.Nombre.Contains(nombre))
-                            .ToListAsync();                        //g.Nombre.StartsWith("C")|| g.Nombre.StartsWith("A")).ToListAsync();
-                                                                    //.OrderBy(g=>g.Nombre)
-        }
-
-        [HttpGet("paginacion")]
-        public async Task<ActionResult<IEnumerable<Genero>>> GetPaginacion(int pagina=1)
-        {
-            var cantidadRegistrosPorPagina = 2;
-            var generos = await _dbContext.Generos
-                                .Skip((pagina-1)*cantidadRegistrosPorPagina)
-                                .Take(cantidadRegistrosPorPagina)
-                                .ToListAsync();
-
-            return generos;
+            return Ok();
         }
 
     }
